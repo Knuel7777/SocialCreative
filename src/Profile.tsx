1e56c717e74d59f2c921a2575c8485cd348cc859
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2024-02-20 20:40:40 -0600

    Create profile component

---
+++ src/Profile.tsx
@@ -0,0 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Profile() {
+    
+    return(
+        <section ClassNameName="container mt-5">
+            <section className='row justify-content-center'>
+                
+                <section className='col-md-4'>
+                    <section ClassName="card" style="width: 18rem;">
+                        <img ClassName="card-img-top" src="..." alt="Card image cap"/>
+                        <section ClassName="card-body">
+                            <h5 ClassName="card-title">Card title</h5>
+                            <p ClassName="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" ClassName="btn btn-primary">Go somewhere</a>
+                        </section>
+                    </section>
+                </section>
+
+                <section className='col-md-4'>
+                    <section ClassName="card" style="width: 18rem;">
+                        <img ClassName="card-img-top" src="..." alt="Card image cap"/>
+                        <section ClassName="card-body">
+                            <h5 ClassName="card-title">Card title</h5>
+                            <p ClassName="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" ClassName="btn btn-primary">Go somewhere</a>
+                        </section>
+                    </section>
+                </section>s
+
+            </section>
+        </section>
+    )
+}
+
+export default Profile;
