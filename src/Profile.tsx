e8373a899e422f99ecdc8622a2f3c7f0a26b896d
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2024-02-22 01:17:08 -0600

    Add files via upload
    Updating the grid.

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -2,34 +2,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Profile() {
   return(
- <>
-<section className="container-fluid mt-5">
-  <section className="row justify-content-center">
-    <section className="col-md-6 text-center">
-      <section className="picture">
-        <h5>Picture</h5>
-        <img src="" className="img-fluid" style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
-        <input type="file" accept="image/*" className="form-control-file mt-3" />
-        <button className="btn btn-primary mt-3">Update Picture</button>
-      </section>
+
+<section className="container-fluid">
+  <section className="row text-center">
+
+    <section className="col mx-auto">
+      <h5>Picture</h5>
+      <img src="" className="img-fluid" style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
+      <label className="btn btn-primary mt-3">Update Picture<input type="file" accept="image/*" style={{ display: 'none' }}/></label>
     </section>
     
-    <section className="col-md-6 text-center">
-      <section>
-        <h5>Details</h5>
-        <input type="text" name="name" className="form-control mb-3" placeholder="Name" />
-        <input type="text" name="lastName" className="form-control mb-3" placeholder="Last Name" />
-        <input type="text" name="phoneNumber" className="form-control mb-3" placeholder="Phone Number" />
-        <input type="email" name="email" className="form-control mb-3" placeholder="Email" />
-        <button className="btn btn-primary">Update Info</button>
-      </section>
+    <section className="col mx-auto">
+      <h5>Details</h5>
+      <input type="text" name="name" className="form-control mb-3" placeholder="Name" />
+      <input type="text" name="lastName" className="form-control mb-3" placeholder="Last Name" />
+      <input type="text" name="phoneNumber" className="form-control mb-3" placeholder="Phone Number" />
+      <input type="email" name="email" className="form-control mb-3" placeholder="Email" />
+      <button className="btn btn-primary">Update Info</button>
     </section>
+
   </section>
 </section>
 
-  </>
-
-    )
+  )
 }
 
 export default Profile
