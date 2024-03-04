4bb33422ae8f2fb062c2c572c3ee2cf407217779
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2024-03-03 22:01:18 -0600

    Remake Profile.tsx

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-
 const Profile = ({ onUpdateProfile }) => {
   const [profile, setProfile] = useState({
     name: '',
@@ -11,58 +10,48 @@ const Profile = ({ onUpdateProfile }) => {
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setProfile(prevProfile => ({
-      ...prevProfile,
-      [name]: value
-    }));
-  };
-
-  const handlePictureChange = (e) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setProfile(prevProfile => ({
-          ...prevProfile,
-          picture: reader.result
-        }));
-      };
-      reader.readAsDataURL(file);
+    const { name, value, files } = e.target;
+    if (files) {
+      const file = files[0];
+      setProfile(prevProfile => ({
+        ...prevProfile,
+        picture: URL.createObjectURL(file)
+      }));
+    } else {
+      setProfile(prevProfile => ({
+        ...prevProfile,
+        [name]: value
+      }));
     }
   };
 
   const handleSubmit = () => {
-    const onUpdateProfile = (updatedProfile) => {
-      console.log("Updating profile:", updatedProfile);
-    };
     onUpdateProfile(profile);
   };
 
-  return(
-
+  return (
     <section className="container-fluid">
       <section className="row text-center">
+        <h3 className='mb-3'>Account Details</h3>
 
-        <section className="col mx-auto">
-          <h5>Picture</h5>
-          <img src={profile.picture} className="img-fluid" style={{ width: '200px', height: '200px', objectFit: 'contain' }} />
-          <label className="btn btn-primary mt-3">Upload Picture<input type="file" accept="image/*" onChange={handlePictureChange} style={{ display: 'none' }}/></label>
-        </section>
-        
-        <section className="col mx-auto">
-          <h5>Details</h5>
+        <section className="col-md-3 mx-auto text-center">
           <input type="text" name="name" value={profile.name} onChange={handleChange} className="form-control mb-3" placeholder="Name" />
           <input type="text" name="lastName" value={profile.lastName} onChange={handleChange} className="form-control mb-3" placeholder="Last Name" />
           <input type="text" name="phoneNumber" value={profile.phoneNumber} onChange={handleChange} className="form-control mb-3" placeholder="Phone Number" />
           <input type="email" name="email" value={profile.email} onChange={handleChange} className="form-control mb-3" placeholder="Email" />
-          <button onClick={handleSubmit} className="btn btn-primary">Update Info</button>
+          <label className="btn btn-primary mt-3">Upload Picture<input type="file" accept="image/*" onChange={handleChange} style={{ display: 'none' }} /></label>
         </section>
 
+        <section className="col-md-4 mx-auto text-center">
+          <img src={profile.picture} className="img-fluid mb-2" style={{ width: '200px', height: '200px', objectFit: 'contain' }} alt="Profile" />
+          <p className='mb-0'>{profile.name} {profile.lastName}</p>
+          <p className='mb-0'>{profile.email}</p>
+          <p>{profile.phoneNumber}</p>
+        </section>
+        
       </section>
     </section>
-
-  )
+  );
 }
 
-export default Profile
+export default Profile;
