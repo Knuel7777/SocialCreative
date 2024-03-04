1c91addacfa738b9021e40676f670de051dbd085
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2024-03-03 20:33:46 -0600

    Updating the CSS

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -4,10 +4,25 @@ import { Link } from "react-router-dom";
 function Navbar() {
 
   return (
-    <div>
-      <Link className='navbar-brand' to='/Profile'>Profile</Link>
-      <Link className='navbar-brand' to='/LogIn'>Login</Link>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light p-0">
+        <section className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+
+          <li className="nav-item">
+          <Link className="nav-link" to="/Profile">Profile</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/LogIn">Login</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/Forum">Forum</Link>
+            </li>
+
+          </ul>
+        </section>
+    </nav>
   )
 }
 
