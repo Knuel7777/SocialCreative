980868ce3764a88d740791ba393aa63d7a758d67
brylop15 <brylop15@gmail.com>
2024-03-05 00:33:00 -0600

    Login
    Commit componente Login
    Se agrega:
    Diseño
    Validacion de email de la U
    Validacion de contraseña
    
    Falta:
    Redireccionamiento

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Login from './components/Login/LogIn'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import './assets/react.svg'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
 )
