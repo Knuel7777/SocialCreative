e7b961d66895ff482aab98e2f7af1bc8d29d08c8
brylop15 <brylop15@gmail.com>
2024-03-05 00:36:02 -0600

    Update main.tsx
    Actualizacion del strict mode y desplegar login desde el principio

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import Login from './components/Login/LogIn'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import './assets/react.svg'
-import App from './App'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App/>
+    <Login/>
   </React.StrictMode>,
 )
