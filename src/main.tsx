caffbd331bfaf4a7e2c04ecbf37a1cde428a3267
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2024-03-06 09:36:30 -0600

    Update main.tsx
    Fixing Main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import Login from './components/Login/LogIn'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.js'
-import './assets/react.svg'
 import App from './App.tsx'
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Login/>
+    <App />
   </React.StrictMode>,
 )
