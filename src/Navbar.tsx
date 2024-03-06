d39622301b3ff2174acd970aa6eddd4208c4d7d1
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2024-03-06 10:01:26 -0600

    Update Navbar.tsx

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -7,12 +7,19 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-0">
       <section className="collapse navbar-collapse">
         <ul className="navbar-nav ms-auto">
+
           <li className="nav-item">
-            <Link className="nav-link text-white" to="/Profile">Profile</Link>
+            <Link className="nav-link text-white" to="/LogIn">Login</Link>
           </li>
+
           <li className="nav-item">
             <Link className="nav-link text-white" to="/Forum">Forum</Link>
           </li>
+
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/Profile">Profile</Link>
+          </li>
+
         </ul>
       </section>
     </nav>
