528c68a4fb7242b25927359c8e0f3214a986856f
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2024-03-06 09:57:58 -0600

    Update - Cosmetic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import './assets/react.svg'
 import Navbar from './Navbar';
 import LogIn from './LogIn';
 import Forum from './Forum';
 import Profile from './Profile';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.js'
-import './assets/react.svg'
-
 
 function App() {
 
