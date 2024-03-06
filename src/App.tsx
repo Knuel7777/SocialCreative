039cefb1c98b36fbe8e58bd7ea4b49e9d93b50cc
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2024-03-06 09:53:29 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Navbar';
-import Profile from './Profile';
+import LogIn from './LogIn';
 import Forum from './Forum';
+import Profile from './Profile';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import './assets/react.svg'
+
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+        <Navbar/>
         <Routes>
-          <Route path="/forum" element={<Forum />} />
+          <Route path='/Profile' element={<Profile />} />
+          <Route path='/Login' element={<LogIn />} />
+		      <Route path="/forum" element={<Forum />} />
         </Routes>
       </BrowserRouter>
-    </>
-
+      </>
+    
   )
 }
 
