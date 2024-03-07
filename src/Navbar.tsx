5dfa6026a964ed963f461fc78729eeb08f972328
carloslo1 <71738382+carloslo1@users.noreply.github.com>
2024-03-06 21:28:33 -0600

    Update Navbar.tsx

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,29 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+  const location = useLocation(); 
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-0">
       <section className="collapse navbar-collapse">
         <ul className="navbar-nav ms-auto">
+        
+          {location.pathname === "/Forum" && (
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/Profile">Profile</Link>
+            </li>
+          )}
 
-          <li className="nav-item">
-            <Link className="nav-link text-white" to="/LogIn">Login</Link>
-          </li>
+          {location.pathname === "/Profile" && (
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/Forum">Forum</Link>
+            </li>
+          )}
 
-          <li className="nav-item">
-            <Link className="nav-link text-white" to="/Forum">Forum</Link>
-          </li>
-
-          <li className="nav-item">
-            <Link className="nav-link text-white" to="/Profile">Profile</Link>
-          </li>
-
+          {
+            (location.pathname === "/Forum" || location.pathname === "/Profile") && (
+              <li className="nav-item">
+                <Link className="nav-link text-white" to="/LogIn">Signout</Link>
+              </li>
+            )
+          }
         </ul>
       </section>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
